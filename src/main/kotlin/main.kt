7c44d14d4897fbd392400b9945418301c0dea528
lagashinani <lagashina.ni@sch2033.ru>
2021-10-20 10:06:38 +0300

    init

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,14 @@
+package ru.netology
+fun main() {
+    var amount = 200
+    var amount1 = amount*0.75
+    if (amount1>3500){
+        println(amount1)
+    }
+    else {
+        println(3500)
+    }
+}
+
+
+
